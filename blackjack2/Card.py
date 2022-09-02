5aa860c0522f38d358d1252df351bbcec62751dd
jfain11 <59984623+jfain11@users.noreply.github.com>
2022-09-02 13:13:30 -0400

    Add files via upload

---
+++ blackjack2/Card.py
@@ -0,0 +1,29 @@
+class Card:
+    _suit: str
+    _value: int
+
+    def __init__(self, _value: int, _suit: str):
+        """
+        :param _value: the integer value of the card
+        :param _suit: string representation
+        """
+        pass
+
+    def filename(self) -> str:
+        """
+        generates the filename of the image that corresponds to the card
+        :return: the filename of the image as a string
+        """
+        pass
+
+    def cardValue(self) -> int:
+        """
+        :return: the value of the card as an integer
+        """
+        pass
+
+    def cardSuit(self) -> str:
+        """
+        :return: returns the suit of the card as a string. ex: "c"
+        """
+        pass
---
+++ blackjack2/CardDeck.py
@@ -0,0 +1,28 @@
+import Card
+
+class CardDeck:
+    _cards: list
+
+    def __init__(self,):
+        pass
+
+    def freshDeck(self):
+        """
+        creates a fresh un-shuffled 52 card deck
+        :return: None
+        """
+        pass
+
+    def shuffle(self):
+        """
+        shuffles the deck of cards
+        :return: None
+        """
+        pass
+
+    def dealOne(self) -> Card:
+        """
+        deals one off the top of the deck and removes it from the deck
+        :return: the card off the top of the deck; in position 0 of the cards list
+        """
+        pass
